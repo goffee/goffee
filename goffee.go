f41a91f4ff8e352782cdaf1aae3f1866ca4b5428
Stephen Sykes <sdsykes@gmail.com>
2015-01-24 11:25:28 +0200

    Fetch a page via tor proxy

--- goffee.go
+++ goffee.go
@@ -1,7 +1,12 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/gophergala/goffee/tor"
+)
 
 func main() {
 	fmt.Println("Hello, Gopher Gala!")
+	body, _ := tor.TorGet("http://www.kiskolabs.com")
+        fmt.Printf("Result was '%s'\n", string(body))
 }
---
+++ tor/tor.go
@@ -0,0 +1,37 @@
+package tor
+
+import (
+  "net/http"
+  "github.com/hailiang/gosocks"
+  "io/ioutil"
+)
+
+func prepareProxyClient() *http.Client {
+  	dialSocksProxy := socks.DialSocksProxy(socks.SOCKS5, "127.0.0.1:9150")
+  	transport := &http.Transport{Dial: dialSocksProxy}
+  	return &http.Client{Transport: transport}
+}
+
+func httpGet(httpClient *http.Client, url string) (resp *http.Response, err error) {
+  	req, err := http.NewRequest("GET", url, nil)
+  	req.Header.Set("User-Agent", "curl/7.21.4 (universal-apple-darwin11.0) libcurl/7.21.4 OpenSSL/0.9.8x zlib/1.2.5")
+  	resp, err = httpClient.Do(req)
+  	return
+}
+
+func httpGetBody(httpClient *http.Client, url string) (body string, err error) {
+  	resp, err := httpGet(httpClient, url)
+	if err != nil {
+		return "", err
+	}
+	defer resp.Body.Close()
+  	bodyb, err := ioutil.ReadAll(resp.Body)
+  	body = string(bodyb)
+  	return
+}
+
+func TorGet(url string) (body string, err error) {
+        clientPtr := prepareProxyClient()
+        body, err = httpGetBody(clientPtr, url)
+	return
+}
