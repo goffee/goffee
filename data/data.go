777289c17a05c3271cf62169a5d02411f2ed06cc
Joao Carlos <mail@joao-carlos.com>
2015-01-24 13:21:45 +0200

    Make it possible to create and fetch results and checks

--- data/data.go
+++ data/data.go
@@ -13,15 +13,21 @@ import (
 var db gorm.DB
 
 type Check struct {
-	Id  int64
-	URL string
+	Id        int64
+	URL       string `gorm:"column:url"`
+	Status    int    // status code of last result
+	Success   bool   // success status of last result
+	CreatedAt time.Time
+	UpdatedAt time.Time
 }
 
 type Result struct {
-	Timestamp time.Time
+	Id        int64
+	CreatedAt time.Time
 	Status    int
 	Success   bool
-	IP        string
+	IP        string `gorm:"column:ip"`
+	CheckId   int64
 }
 
 func InitDatabase() (err error) {
@@ -35,13 +41,41 @@ func InitDatabase() (err error) {
 	return nil
 }
 
-func (c Check) Create() error {
-	r := db.Create(c)
-	return r.Error
+func Checks() ([]Check, error) {
+	var checks []Check
+	res := db.Find(&checks)
+	return checks, res.Error
 }
 
-func Checks() ([]Check, error) {
-	checks := []Check{}
-	r := db.Find(&checks)
-	return checks, r.Error
+func (c *Check) Create() error {
+	res := db.Create(c)
+	return res.Error
+}
+
+func (c *Check) AddResult(r *Result) error {
+	tx := db.Begin()
+
+	r.CheckId = c.Id
+	res := tx.Create(r)
+	if res.Error != nil {
+		tx.Rollback()
+		return res.Error
+	}
+
+	c.Status = r.Status
+	c.Success = r.Success
+	res = tx.Save(c)
+	if res.Error != nil {
+		tx.Rollback()
+		return res.Error
+	}
+
+	tx.Commit()
+	return nil
+}
+
+func (c *Check) Results() ([]Result, error) {
+	var results []Result
+	res := db.Model(c).Related(&results)
+	return results, res.Error
 }
