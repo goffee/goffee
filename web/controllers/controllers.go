eccc4e788343fbe70bea7fff6620b726ed34526d
Matias Korhonen <matias@kiskolabs.com>
2015-01-24 16:35:17 +0200

    Implements some basic user stuff

--- web/controllers/controllers.go
+++ web/controllers/controllers.go
@@ -3,8 +3,27 @@ package controllers
 import (
 	"net/http"
 
+	"github.com/gophergala/goffee/data"
 	"github.com/gophergala/goffee/web/render"
+	"github.com/gorilla/sessions"
+	"github.com/zenazn/goji/web"
 )
+
+func currentSession(c web.C) *sessions.Session {
+	session := c.Env["Session"].(*sessions.Session)
+	return session
+}
+
+func currentUser(c web.C) (data.User, error) {
+	session := currentSession(c)
+	userID := session.Values["UserId"].(int64)
+	return data.FindUser(userID)
+}
+
+func userSignedIn(c web.C) bool {
+	_, err := currentUser(c)
+	return err != nil
+}
 
 // Home serves the home page
 func Home(w http.ResponseWriter, req *http.Request) {
