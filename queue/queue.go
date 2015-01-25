931e3b8a61ab090008a743a02724ff3cf1529289
Joao Carlos <mail@joao-carlos.com>
2015-01-25 22:44:27 +0200

    Remove unused stuff

--- queue/queue.go
+++ queue/queue.go
@@ -106,10 +106,6 @@ func newPool(server string) *redis.Pool {
 			if err != nil {
 				return nil, err
 			}
-			// if _, err := c.Do("AUTH", password); err != nil {
-			// 	c.Close()
-			// 	return nil, err
-			// }
 			return c, err
 		},
 		TestOnBorrow: func(c redis.Conn, t time.Time) error {
