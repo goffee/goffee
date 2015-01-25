aa6d3d954258665d7678c7264c2853f7f7bf31d8
Joao Carlos <mail@joao-carlos.com>
2015-01-25 15:11:40 +0200

    Fix flag parsing

--- goffee.go
+++ goffee.go
@@ -2,11 +2,15 @@ package main
 
 import (
 	"flag"
+	"log"
 
+	"github.com/gophergala/goffee/Godeps/_workspace/src/golang.org/x/oauth2"
+	"github.com/gophergala/goffee/Godeps/_workspace/src/golang.org/x/oauth2/github"
 	"github.com/gophergala/goffee/probe"
 	"github.com/gophergala/goffee/queue"
 	"github.com/gophergala/goffee/scheduler"
 	"github.com/gophergala/goffee/web"
+	"github.com/gophergala/goffee/web/controllers"
 	"github.com/gophergala/goffee/writer"
 )
 
@@ -18,13 +22,31 @@ var redisAddress string
 var bind string
 
 func init() {
+	var gitHubClientID string
+	var gitHubClientSecret string
+
 	flag.BoolVar(&webMode, "webmode", false, "Run goffee in webmode")
 	flag.BoolVar(&probeMode, "torfetch", false, "Fetch something via Tor")
 	flag.BoolVar(&schedulerMode, "scheduler", false, "Run goffee scheduler")
 	flag.BoolVar(&writerMode, "writer", false, "Run goffee writer")
 	flag.StringVar(&redisAddress, "redisaddress", "127.0.0.1:6379", "Address of redis including port")
 	flag.StringVar(&bind, "bind", "127.0.0.1:8000", "Address to bind to")
+	flag.StringVar(&gitHubClientID, "clientid", "", "Github client ID")
+	flag.StringVar(&gitHubClientSecret, "secret", "", "GitHub client Secret")
 	flag.Parse()
+
+	flag.Parse()
+
+	if gitHubClientID == "" || gitHubClientSecret == "" {
+		log.Fatal("No clientid or secret set!")
+	}
+
+	controllers.OAuthConf = &oauth2.Config{
+		ClientID:     gitHubClientID,
+		ClientSecret: gitHubClientSecret,
+		Scopes:       []string{},
+		Endpoint:     github.Endpoint,
+	}
 
 	// If no mode has been defined, just launch them all!
 	if !webMode && !probeMode && !schedulerMode && !writerMode {
@@ -43,7 +65,7 @@ func main() {
 	if probeMode || schedulerMode || writerMode {
 		queue.RedisAddressWithPort = redisAddress
 	}
-	
+
 	if probeMode {
 		probe.Run()
 	}
--- web/controllers/oauth.go
+++ web/controllers/oauth.go
@@ -1,45 +1,21 @@
 package controllers
 
 import (
-	"flag"
-	"log"
 	"net/http"
 
 	"github.com/gophergala/goffee/Godeps/_workspace/src/github.com/google/go-github/github"
 	"github.com/gophergala/goffee/Godeps/_workspace/src/github.com/gorilla/sessions"
 	"github.com/gophergala/goffee/Godeps/_workspace/src/github.com/zenazn/goji/web"
 	"github.com/gophergala/goffee/Godeps/_workspace/src/golang.org/x/oauth2"
-	oauth2github "github.com/gophergala/goffee/Godeps/_workspace/src/golang.org/x/oauth2/github"
 	"github.com/gophergala/goffee/data"
 	"github.com/gophergala/goffee/web/helpers"
 )
 
-var conf *oauth2.Config
-
-func init() {
-	var gitHubClientID string
-	var gitHubClientSecret string
-
-	flag.StringVar(&gitHubClientID, "clientid", "", "Github client ID")
-	flag.StringVar(&gitHubClientSecret, "secret", "", "GitHub client Secret")
-
-	flag.Parse()
-
-	if gitHubClientID == "" || gitHubClientSecret == "" {
-		log.Fatal("No clientid or secret set!")
-	}
-
-	conf = &oauth2.Config{
-		ClientID:     gitHubClientID,
-		ClientSecret: gitHubClientSecret,
-		Scopes:       []string{},
-		Endpoint:     oauth2github.Endpoint,
-	}
-}
+var OAuthConf *oauth2.Config
 
 // OAuthAuthorize makes the user login
 func OAuthAuthorize(w http.ResponseWriter, req *http.Request) {
-	url := conf.AuthCodeURL("state", oauth2.AccessTypeOnline)
+	url := OAuthConf.AuthCodeURL("state", oauth2.AccessTypeOnline)
 	http.Redirect(w, req, url, http.StatusFound)
 }
 
@@ -57,7 +33,7 @@ func OAuthCallback(c web.C, w http.ResponseWriter, req *http.Request) {
 	code := req.FormValue("code")
 
 	// Exchange the received code for a token
-	token, err := conf.Exchange(oauth2.NoContext, code)
+	token, err := OAuthConf.Exchange(oauth2.NoContext, code)
 	if err != nil {
 		renderError(c, w, req, "Authentication failed", http.StatusUnauthorized)
 		return
@@ -65,7 +41,7 @@ func OAuthCallback(c web.C, w http.ResponseWriter, req *http.Request) {
 
 	session := c.Env["Session"].(*sessions.Session)
 
-	httpClient := conf.Client(oauth2.NoContext, token)
+	httpClient := OAuthConf.Client(oauth2.NoContext, token)
 	githubClient := github.NewClient(httpClient)
 	user, _, err := githubClient.Users.Get("")
 
